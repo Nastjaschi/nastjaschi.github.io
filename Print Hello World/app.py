c39808eab28015c69f8ec1640102522c75ec3e80
Nastjaschi <94838825+Nastjaschi@users.noreply.github.com>
2022-09-18 10:07:19 +0300

    Add files via upload

--- Print Hello World/app.py
+++ Print Hello World/app.py
@@ -2,14 +2,38 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
+
+
 @app.route("/")
 def home():
 
- 
-
     return render_template('main_unauthorized.html')
 
-@app.route("/loggedin")
+
+@app.route("/loggedin/")
 def logged_main():
 
     return render_template('main_logged.html')
+
+
+@app.route("/admin/")
+def logged_admin():
+
+    return render_template('main_admin.html')
+
+
+@app.route("/review_form/")
+def review_form():
+
+    return render_template('leave_review.html')
+    
+@app.route("/new_course/")
+def new_course():
+
+    return render_template('new_course.html')
+
+
+@app.route("/admin_new_courses/")
+def admin_new_courses():
+
+    return render_template('admin_new_courses.html')
